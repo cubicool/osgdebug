2484e2563d224f106615430d0f36880ae226f0e1
Jeremy Moles <cubicool@gmail.com>
2026-07-22 10:52:05 -0400

    Adds `osgx` as its OWN Python module

--- ext/osgDebug-python.cpp
+++ ext/osgDebug-python.cpp
@@ -17,9 +17,6 @@ namespace pyx = pybind11x;
 PYBIND11_MODULE(osgDebug, m) {
 	auto py_osg = py::module_::import("OpenSceneGraph");
 
-	osgx::pbr::registerShaderLibs();
-	osgx::ibl::registerShaderLibs();
-
 	py::class_<
 		osgDebug::GraphicsOperation,
 		osg::GraphicsOperation,
@@ -131,8 +128,8 @@ PYBIND11_MODULE(osgDebug, m) {
 			py::init<GLuint, std::string_view, osgDebug::Source, bool>(),
 			py::arg("id"),
 			py::arg("message"),
-			py::arg("source") = osgDebug::Source::APPLICATION,
-			py::arg("measureTime") = false
+			py::arg("source")=osgDebug::Source::APPLICATION,
+			py::arg("measureTime")=false
 		)
 		.def("__enter__", [](osgDebug::Scoped& self) -> osgDebug::Scoped& {
 			return self;
@@ -146,173 +143,6 @@ PYBIND11_MODULE(osgDebug, m) {
 			return false; // don't suppress Python exceptions
 		})
 	;
-
-	// osgx::Grid -- exposed here because osgDebug is the project-level Python module;
-	// the base osg::Geometry and Vec types are supplied by OpenSceneGraph.
-	auto grid = py::class_<
-		osgx::Grid,
-		osg::Geometry,
-		osg::ref_ptr<osgx::Grid>
-	>(m, "Grid");
-
-	py::enum_<osgx::Grid::EdgeMode>(grid, "EdgeMode")
-		.value("EDGE_ASIS", osgx::Grid::EDGE_ASIS)
-		.value("EDGE_HIDE", osgx::Grid::EDGE_HIDE)
-		.value("EDGE_NUDGE", osgx::Grid::EDGE_NUDGE)
-		.export_values()
-	;
-
-	py::enum_<osgx::Grid::LineMode>(grid, "LineMode")
-		.value("LINE_SCREEN_PIXELS", osgx::Grid::LINE_SCREEN_PIXELS)
-		.value("LINE_GRID_UNITS", osgx::Grid::LINE_GRID_UNITS)
-		.export_values()
-	;
-
-	grid
-		.def(py::init<>())
-		.def(py::init<const osg::Vec3&, const osg::Vec3&, const osg::Vec3&>(),
-			"corner"_a, "width_vec"_a, "height_vec"_a
-		)
-		.def_property("canvasSize", &osgx::Grid::getCanvasSize, &osgx::Grid::setCanvasSize)
-		.def_property("gridInterval", &osgx::Grid::getGridInterval, &osgx::Grid::setGridInterval)
-		.def_property(
-			"gridIntervalStrong",
-			&osgx::Grid::getGridIntervalStrong,
-			&osgx::Grid::setGridIntervalStrong
-		)
-		.def_property("lineWidthPx", &osgx::Grid::getLineWidthPx, &osgx::Grid::setLineWidthPx)
-		.def_property("lineWidth", &osgx::Grid::getLineWidth, &osgx::Grid::setLineWidth)
-		.def_property("edgeMode", &osgx::Grid::getEdgeMode, &osgx::Grid::setEdgeMode)
-		.def_property("lineMode", &osgx::Grid::getLineMode, &osgx::Grid::setLineMode)
-		.def_property("colorBg", &osgx::Grid::getColorBg, &osgx::Grid::setColorBg)
-		.def_property("colorLine", &osgx::Grid::getColorLine, &osgx::Grid::setColorLine)
-		.def_property(
-			"colorLineStrong",
-			&osgx::Grid::getColorLineStrong,
-			&osgx::Grid::setColorLineStrong
-		)
-		.def("orthoCamera", &osgx::Grid::orthoCamera)
-		.def_static("createOrthoCamera", py::overload_cast<>(&osgx::Grid::createOrthoCamera))
-		.def_static(
-			"createOrthoCamera",
-			py::overload_cast<const osg::Vec3&, const osg::Vec3&, const osg::Vec3&>(
-				&osgx::Grid::createOrthoCamera
-			),
-			"corner"_a, "width_vec"_a, "height_vec"_a
-		)
-	;
-
-	// osgx::pbr / osgx::ibl -- exposed here for the same reason as osgx::Grid above (osgDebug is
-	// the project-level Python module). Ported from the STATIC path of pyosg-lighting/09-ibl.py
-	// and already proven in osgSlug's osgslug-pbr-ibl.cpp; the goal is for Python demos to reuse
-	// this toolkit (GLSL snippets + resolveShaderLibs() + the cubemap/BRDF-LUT/SH9 host-side
-	// helpers) instead of re-deriving the shader/UBO plumbing from scratch each time.
-	m.def(
-		"resolveShaderLibs",
-		&osgx::resolveShaderLibs,
-		"src"_a,
-		"Expand '#pragma osgx::pbr ...' / '#pragma osgx::ibl ...' lines into their GLSL source."
-	);
-
-	auto m_pbr = m.def_submodule("pbr", "osgx::pbr -- BRDF math GLSL snippets + direct-light rig");
-
-	m_pbr.attr("D_GGX") = osgx::pbr::D_GGX;
-	m_pbr.attr("G_SCHLICK") = osgx::pbr::G_SCHLICK;
-	m_pbr.attr("G_SMITH") = osgx::pbr::G_SMITH;
-	m_pbr.attr("F_SCHLICK") = osgx::pbr::F_SCHLICK;
-	m_pbr.attr("F_SCHLICK_ROUGHNESS") = osgx::pbr::F_SCHLICK_ROUGHNESS;
-	m_pbr.attr("DIRECT_SPECULAR") = osgx::pbr::DIRECT_SPECULAR;
-	m_pbr.attr("F_MULTISCATTER") = osgx::pbr::F_MULTISCATTER;
-	m_pbr.attr("IBL_SPECULAR") = osgx::pbr::IBL_SPECULAR;
-	m_pbr.attr("TONEMAP_PBR_NEUTRAL") = osgx::pbr::TONEMAP_PBR_NEUTRAL;
-
-	m_pbr.def("snippets", &osgx::pbr::snippets);
-
-	py::class_<osgx::pbr::OrbitLightRig::Orbit>(m_pbr, "Orbit")
-		.def(
-			py::init([](float radius, float height, float speed, float phase, float intensity) {
-				return osgx::pbr::OrbitLightRig::Orbit{radius, height, speed, phase, intensity};
-			}),
-			"radius"_a = 0.5f,
-			"height"_a = 0.5f,
-			"speed"_a = 0.5f,
-			"phase"_a = 0.0f,
-			"intensity"_a = 1.0f
-		)
-		.def_readwrite("radius", &osgx::pbr::OrbitLightRig::Orbit::radius)
-		.def_readwrite("height", &osgx::pbr::OrbitLightRig::Orbit::height)
-		.def_readwrite("speed", &osgx::pbr::OrbitLightRig::Orbit::speed)
-		.def_readwrite("phase", &osgx::pbr::OrbitLightRig::Orbit::phase)
-		.def_readwrite("intensity", &osgx::pbr::OrbitLightRig::Orbit::intensity)
-	;
-
-	py::class_<
-		osgx::pbr::OrbitLightRig,
-		osg::NodeCallback,
-		osg::ref_ptr<osgx::pbr::OrbitLightRig>
-	>(m_pbr, "OrbitLightRig")
-		.def(py::init<>())
-		.def_readwrite("ss", &osgx::pbr::OrbitLightRig::ss)
-		.def_readwrite("center", &osgx::pbr::OrbitLightRig::center)
-		.def_readwrite("intensity", &osgx::pbr::OrbitLightRig::intensity)
-		.def_readwrite("uniformName", &osgx::pbr::OrbitLightRig::uniformName)
-		.def_readwrite("orbits", &osgx::pbr::OrbitLightRig::orbits)
-	;
-
-	auto m_ibl = m.def_submodule("ibl", "osgx::ibl -- prefiltered cubemap + BRDF LUT + SH9 diffuse");
-
-	m_ibl.attr("FULLSCREEN_VERT") = osgx::ibl::FULLSCREEN_VERT;
-	m_ibl.attr("BRDF_LUT_FRAG") = osgx::ibl::BRDF_LUT_FRAG;
-	m_ibl.attr("SH_IRRADIANCE") = osgx::ibl::SH_IRRADIANCE;
-
-	py::class_<
-		osgx::ibl::RunOnceCallback,
-		osg::NodeCallback,
-		osg::ref_ptr<osgx::ibl::RunOnceCallback>
-	>(m_ibl, "RunOnceCallback")
-		.def(py::init<>())
-		.def("rebake", &osgx::ibl::RunOnceCallback::rebake, "node"_a)
-	;
-
-	m_ibl
-		.def(
-			"loadPrefilterCubemap",
-			&osgx::ibl::loadPrefilterCubemap,
-			"path"_a,
-			"Loads a pre-baked GGX-prefiltered cubemap (.ktx2); returns None (and logs OSG_WARN) "
-			"if the path doesn't load as a TextureCubeMap."
-		)
-		.def(
-			"makeBRDFLUTCamera",
-			&osgx::ibl::makeBRDFLUTCamera,
-			"lutSize"_a,
-			"lut"_a,
-			"Configures `lut` in-place (size/format/filters) and returns a PRE_RENDER camera that "
-			"bakes the split-sum BRDF LUT into it exactly once (see RunOnceCallback)."
-		)
-	;
-
-	py::class_<osgx::ibl::SH9>(m_ibl, "SH9")
-		.def(py::init<>())
-		.def("__len__", [](const osgx::ibl::SH9&) { return 9; })
-		.def("__getitem__", [](const osgx::ibl::SH9& self, size_t i) {
-			if(i >= 9) throw py::index_error();
-
-			return self.coeffs[i];
-		})
-		.def("__setitem__", [](osgx::ibl::SH9& self, size_t i, const osg::Vec3f& v) {
-			if(i >= 9) throw py::index_error();
-
-			self.coeffs[i] = v;
-		})
-	;
-
-	m_ibl.def(
-		"computeSH",
-		&osgx::ibl::computeSH,
-		"image"_a,
-		"Projects an equirectangular HDR/LDR osg.Image onto SH9 diffuse irradiance coefficients."
-	);
 
 	// osgDebug::imgui -- deliberately NOT a general ImGui wrapper (that's pyimgui's
 	// job elsewhere). Just enough to build quick debugging knobs inside a
@@ -343,8 +173,8 @@ PYBIND11_MODULE(osgDebug, m) {
 	py::class_<osgDebug::imgui::SectionOptions>(m_imgui, "SectionOptions")
 		.def(
 			py::init(&osgDebug::imgui::makeSectionOptions),
-			"expand"_a = false,
-			"default_open"_a = false
+			"expand"_a=false,
+			"default_open"_a=false
 		)
 		.def_readwrite("expand", &osgDebug::imgui::SectionOptions::expand)
 		.def_readwrite("default_open", &osgDebug::imgui::SectionOptions::defaultOpen)
@@ -352,13 +182,43 @@ PYBIND11_MODULE(osgDebug, m) {
 
 	py::class_<osgDebug::imgui::Panel>(m_imgui, "Panel")
 		.def(py::init<>())
-		.def("addSection", &osgDebug::imgui::Panel::addSection, "label"_a, "fn"_a, "options"_a = osgDebug::imgui::SectionOptions())
+		.def(
+			"addSection", &osgDebug::imgui::Panel::addSection,
+			"label"_a,
+			"fn"_a,
+			"options"_a=osgDebug::imgui::SectionOptions()
+		)
 		.def("removeSection", &osgDebug::imgui::Panel::removeSection, "label"_a)
 		.def("clearSections", &osgDebug::imgui::Panel::clearSections)
-		.def("addStatsSection", &osgDebug::imgui::Panel::addStatsSection, "viewer"_a, "default_open"_a = false)
-		.def("addProfilerSection", &osgDebug::imgui::Panel::addProfilerSection, "view"_a, "sceneRoot"_a, "default_open"_a = false)
-		.def("addTextureSection", py::overload_cast<osgViewer::View&, osg::Node*, bool>(&osgDebug::imgui::Panel::addTextureSection), "view"_a, "root"_a, "default_open"_a = false)
-		.def("addTextureSection", py::overload_cast<osgViewer::View&, bool>(&osgDebug::imgui::Panel::addTextureSection), "view"_a, "default_open"_a = false)
+		.def(
+			"addStatsSection",
+			&osgDebug::imgui::Panel::addStatsSection,
+			"viewer"_a,
+			"default_open"_a=false
+		)
+		.def(
+			"addProfilerSection",
+			&osgDebug::imgui::Panel::addProfilerSection,
+			"view"_a,
+			"sceneRoot"_a,
+			"default_open"_a=false
+		)
+		.def(
+			"addTextureSection",
+			py::overload_cast<
+				osgViewer::View&,
+				osg::Node*, bool
+			>(&osgDebug::imgui::Panel::addTextureSection),
+			"view"_a,
+			"root"_a,
+			"default_open"_a=false
+		)
+		.def(
+			"addTextureSection",
+			py::overload_cast<osgViewer::View&, bool>(&osgDebug::imgui::Panel::addTextureSection),
+			"view"_a,
+			"default_open"_a=false
+		)
 		.def("draw", &osgDebug::imgui::Panel::draw, "render_info"_a)
 	;
 
@@ -374,15 +234,15 @@ PYBIND11_MODULE(osgDebug, m) {
 			// Viewer's py::class_ lists it as a base).
 			py::init<osgViewer::View&, osg::Camera*, osgDebug::imgui::Options>(),
 			"viewer"_a,
-			"draw_camera"_a = nullptr,
-			"options"_a = osgDebug::imgui::Options()
+			"draw_camera"_a=nullptr,
+			"options"_a=osgDebug::imgui::Options()
 		)
 		.def(
 			"addSection",
 			&osgDebug::imgui::Widget::addSection,
 			"label"_a,
 			"fn"_a,
-			"options"_a = osgDebug::imgui::SectionOptions()
+			"options"_a=osgDebug::imgui::SectionOptions()
 		)
 		.def("removeSection", &osgDebug::imgui::Widget::removeSection, "label"_a)
 		.def("clearSections", &osgDebug::imgui::Widget::clearSections)
@@ -393,14 +253,14 @@ PYBIND11_MODULE(osgDebug, m) {
 			"addStatsSection",
 			&osgDebug::imgui::Widget::addStatsSection,
 			"viewer"_a,
-			"default_open"_a = false
+			"default_open"_a=false
 		)
 		.def(
 			"addProfilerSection",
 			&osgDebug::imgui::Widget::addProfilerSection,
 			"view"_a,
 			"sceneRoot"_a,
-			"default_open"_a = false
+			"default_open"_a=false
 		)
 		.def(
 			"addTextureSection",
@@ -409,13 +269,13 @@ PYBIND11_MODULE(osgDebug, m) {
 			),
 			"view"_a,
 			"root"_a,
-			"default_open"_a = false
+			"default_open"_a=false
 		)
 		.def(
 			"addTextureSection",
 			py::overload_cast<osgViewer::View&, bool>(&osgDebug::imgui::Widget::addTextureSection),
 			"view"_a,
-			"default_open"_a = false
+			"default_open"_a=false
 		)
 	;
 
@@ -427,7 +287,7 @@ PYBIND11_MODULE(osgDebug, m) {
 			"value"_a,
 			"min"_a,
 			"max"_a,
-			"format"_a = "%.3f"
+			"format"_a="%.3f"
 		)
 		.def(
 			"slider_float_nudge",
@@ -436,8 +296,8 @@ PYBIND11_MODULE(osgDebug, m) {
 			"value"_a,
 			"min"_a,
 			"max"_a,
-			"step_pct"_a = 0.01f,
-			"format"_a = "%.3f"
+			"step_pct"_a=0.01f,
+			"format"_a="%.3f"
 		)
 		.def("text", &osgDebug::imgui::text, "text"_a)
 		.def("separator", &osgDebug::imgui::separator)
@@ -452,15 +312,15 @@ PYBIND11_MODULE(osgDebug, m) {
 			&osgDebug::imgui::inputText,
 			"label"_a,
 			"value"_a,
-			"max_length"_a = 256,
-			"enter_returns_true"_a = false
+			"max_length"_a=256,
+			"enter_returns_true"_a=false
 		)
 		.def(
 			"radio_group",
 			&osgDebug::imgui::radioGroup,
 			"value"_a,
 			"labels"_a,
-			"same_line"_a = true
+			"same_line"_a=true
 		)
 	;
 #endif // OSGDEBUG_IMGUI
---
+++ ext/osgx-python.cpp
@@ -0,0 +1,184 @@
+//vimrun! ./test.py
+
+#include "osgx.hpp"
+
+#include "pyosg/pyosg.hpp"
+
+#include "pybind11x.hpp"
+
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+namespace pyx = pybind11x;
+
+PYBIND11_MODULE(osgx, m) {
+	auto py_osg = py::module_::import("OpenSceneGraph");
+
+	osgx::pbr::registerShaderLibs();
+	osgx::ibl::registerShaderLibs();
+
+	auto grid = py::class_<
+		osgx::Grid,
+		osg::Geometry,
+		osg::ref_ptr<osgx::Grid>
+	>(m, "Grid");
+
+	py::enum_<osgx::Grid::EdgeMode>(grid, "EdgeMode")
+		.value("EDGE_ASIS", osgx::Grid::EDGE_ASIS)
+		.value("EDGE_HIDE", osgx::Grid::EDGE_HIDE)
+		.value("EDGE_NUDGE", osgx::Grid::EDGE_NUDGE)
+		.export_values()
+	;
+
+	py::enum_<osgx::Grid::LineMode>(grid, "LineMode")
+		.value("LINE_SCREEN_PIXELS", osgx::Grid::LINE_SCREEN_PIXELS)
+		.value("LINE_GRID_UNITS", osgx::Grid::LINE_GRID_UNITS)
+		.export_values()
+	;
+
+	grid
+		.def(py::init<>())
+		.def(py::init<const osg::Vec3&, const osg::Vec3&, const osg::Vec3&>(),
+			"corner"_a, "width_vec"_a, "height_vec"_a
+		)
+		.def_property("canvasSize", &osgx::Grid::getCanvasSize, &osgx::Grid::setCanvasSize)
+		.def_property("gridInterval", &osgx::Grid::getGridInterval, &osgx::Grid::setGridInterval)
+		.def_property(
+			"gridIntervalStrong",
+			&osgx::Grid::getGridIntervalStrong,
+			&osgx::Grid::setGridIntervalStrong
+		)
+		.def_property("lineWidthPx", &osgx::Grid::getLineWidthPx, &osgx::Grid::setLineWidthPx)
+		.def_property("lineWidth", &osgx::Grid::getLineWidth, &osgx::Grid::setLineWidth)
+		.def_property("edgeMode", &osgx::Grid::getEdgeMode, &osgx::Grid::setEdgeMode)
+		.def_property("lineMode", &osgx::Grid::getLineMode, &osgx::Grid::setLineMode)
+		.def_property("colorBg", &osgx::Grid::getColorBg, &osgx::Grid::setColorBg)
+		.def_property("colorLine", &osgx::Grid::getColorLine, &osgx::Grid::setColorLine)
+		.def_property(
+			"colorLineStrong",
+			&osgx::Grid::getColorLineStrong,
+			&osgx::Grid::setColorLineStrong
+		)
+		.def("orthoCamera", &osgx::Grid::orthoCamera)
+		.def_static("createOrthoCamera", py::overload_cast<>(&osgx::Grid::createOrthoCamera))
+		.def_static(
+			"createOrthoCamera",
+			py::overload_cast<const osg::Vec3&, const osg::Vec3&, const osg::Vec3&>(
+				&osgx::Grid::createOrthoCamera
+			),
+			"corner"_a, "width_vec"_a, "height_vec"_a
+		)
+	;
+
+	// osgx::pbr / osgx::ibl - ported from the STATIC path of pyosg-lighting/09-ibl.py and
+	// already proven in osgSlug's osgslug-pbr-ibl.cpp; the goal is for Python demos to reuse
+	// this toolkit (GLSL snippets + resolveShaderLibs() + the cubemap/BRDF-LUT/SH9 host-side
+	// helpers) instead of re-deriving the shader/UBO plumbing from scratch each time.
+	m.def(
+		"resolveShaderLibs",
+		&osgx::resolveShaderLibs,
+		"src"_a,
+		"Expand '#pragma osgx::pbr ...' / '#pragma osgx::ibl ...' lines into their GLSL source."
+	);
+
+	auto m_pbr = m.def_submodule("pbr", "osgx::pbr - BRDF math GLSL snippets + direct-light rig");
+
+	m_pbr.attr("D_GGX") = osgx::pbr::D_GGX;
+	m_pbr.attr("G_SCHLICK") = osgx::pbr::G_SCHLICK;
+	m_pbr.attr("G_SMITH") = osgx::pbr::G_SMITH;
+	m_pbr.attr("F_SCHLICK") = osgx::pbr::F_SCHLICK;
+	m_pbr.attr("F_SCHLICK_ROUGHNESS") = osgx::pbr::F_SCHLICK_ROUGHNESS;
+	m_pbr.attr("DIRECT_SPECULAR") = osgx::pbr::DIRECT_SPECULAR;
+	m_pbr.attr("F_MULTISCATTER") = osgx::pbr::F_MULTISCATTER;
+	m_pbr.attr("IBL_SPECULAR") = osgx::pbr::IBL_SPECULAR;
+	m_pbr.attr("TONEMAP_PBR_NEUTRAL") = osgx::pbr::TONEMAP_PBR_NEUTRAL;
+
+	m_pbr.def("snippets", &osgx::pbr::snippets);
+
+	py::class_<osgx::pbr::OrbitLightRig::Orbit>(m_pbr, "Orbit")
+		.def(
+			py::init([](float radius, float height, float speed, float phase, float intensity) {
+				return osgx::pbr::OrbitLightRig::Orbit{radius, height, speed, phase, intensity};
+			}),
+			"radius"_a=0.5f,
+			"height"_a=0.5f,
+			"speed"_a=0.5f,
+			"phase"_a=0.0f,
+			"intensity"_a=1.0f
+		)
+		.def_readwrite("radius", &osgx::pbr::OrbitLightRig::Orbit::radius)
+		.def_readwrite("height", &osgx::pbr::OrbitLightRig::Orbit::height)
+		.def_readwrite("speed", &osgx::pbr::OrbitLightRig::Orbit::speed)
+		.def_readwrite("phase", &osgx::pbr::OrbitLightRig::Orbit::phase)
+		.def_readwrite("intensity", &osgx::pbr::OrbitLightRig::Orbit::intensity)
+	;
+
+	py::class_<
+		osgx::pbr::OrbitLightRig,
+		osg::NodeCallback,
+		osg::ref_ptr<osgx::pbr::OrbitLightRig>
+	>(m_pbr, "OrbitLightRig")
+		.def(py::init<>())
+		.def_readwrite("ss", &osgx::pbr::OrbitLightRig::ss)
+		.def_readwrite("center", &osgx::pbr::OrbitLightRig::center)
+		.def_readwrite("intensity", &osgx::pbr::OrbitLightRig::intensity)
+		.def_readwrite("uniformName", &osgx::pbr::OrbitLightRig::uniformName)
+		.def_readwrite("orbits", &osgx::pbr::OrbitLightRig::orbits)
+	;
+
+	auto m_ibl = m.def_submodule("ibl", "osgx::ibl - prefiltered cubemap + BRDF LUT + SH9 diffuse");
+
+	m_ibl.attr("FULLSCREEN_VERT") = osgx::ibl::FULLSCREEN_VERT;
+	m_ibl.attr("BRDF_LUT_FRAG") = osgx::ibl::BRDF_LUT_FRAG;
+	m_ibl.attr("SH_IRRADIANCE") = osgx::ibl::SH_IRRADIANCE;
+
+	py::class_<
+		osgx::ibl::RunOnceCallback,
+		osg::NodeCallback,
+		osg::ref_ptr<osgx::ibl::RunOnceCallback>
+	>(m_ibl, "RunOnceCallback")
+		.def(py::init<>())
+		.def("rebake", &osgx::ibl::RunOnceCallback::rebake, "node"_a)
+	;
+
+	m_ibl
+		.def(
+			"loadPrefilterCubemap",
+			&osgx::ibl::loadPrefilterCubemap,
+			"path"_a,
+			"Loads a pre-baked GGX-prefiltered cubemap (.ktx2); returns None (and logs OSG_WARN) "
+			"if the path doesn't load as a TextureCubeMap."
+		)
+		.def(
+			"makeBRDFLUTCamera",
+			&osgx::ibl::makeBRDFLUTCamera,
+			"lutSize"_a,
+			"lut"_a,
+			"Configures `lut` in-place (size/format/filters) and returns a PRE_RENDER camera that "
+			"bakes the split-sum BRDF LUT into it exactly once (see RunOnceCallback)."
+		)
+	;
+
+	py::class_<osgx::ibl::SH9>(m_ibl, "SH9")
+		.def(py::init<>())
+		.def("__len__", [](const osgx::ibl::SH9&) { return 9; })
+		.def("__getitem__", [](const osgx::ibl::SH9& self, size_t i) {
+			if(i >= 9) throw py::index_error();
+
+			return self.coeffs[i];
+		})
+		.def("__setitem__", [](osgx::ibl::SH9& self, size_t i, const osg::Vec3f& v) {
+			if(i >= 9) throw py::index_error();
+
+			self.coeffs[i] = v;
+		})
+	;
+
+	m_ibl.def(
+		"computeSH",
+		&osgx::ibl::computeSH,
+		"image"_a,
+		"Projects an equirectangular HDR/LDR osg.Image onto SH9 diffuse irradiance coefficients."
+	);
+}
